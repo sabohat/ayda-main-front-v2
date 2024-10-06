f28c61a1fd48d52d5ce80d0d3a3428b4dcf7cba4
sabokhatkalandarova <sabohat2802@gmail.com>
2024-10-06 13:52:08 +0500

    feat:disable contentview if not signed in

--- app/(main)/courses/[id]/page.tsx
+++ app/(main)/courses/[id]/page.tsx
@@ -1,6 +1,18 @@
 import { VideoAppWithChatbotComponent } from '@/components/video-app-with-chatbot';
 
-export default function CoursePage () {
+import { auth } from '@/auth'
+import { getChat, getMissingKeys } from '@/app/actions'
+import { Session } from '@/lib/types'
+import { redirect } from 'next/navigation'
+
+export default async function CoursePage () {
+    const session = (await auth()) as Session
+    const missingKeys = await getMissingKeys()
+
+    if (!session?.user) {
+        redirect(`/login`)
+    }
+
     return <div className='flex w-full min-h-[calc(100vh_-_theme(spacing.16))]'>
         <VideoAppWithChatbotComponent />
     </div>
