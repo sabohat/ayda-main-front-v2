49582ad568ef321fc9500950365a5f39f6a128b5
sabokhatkalandarova <sabohat2802@gmail.com>
2024-10-06 13:39:25 +0500

    fix: build errors

--- app/(main)/courses/[id]/page.tsx
+++ app/(main)/courses/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { VideoAppWithChatbotComponent } from '@/components/video-app-with-chatbot';
 
-export default function CoursePage ({ courseData }: { courseData: any }) {
+export default function CoursePage () {
     return <div className='flex w-full min-h-[calc(100vh_-_theme(spacing.16))]'>
         <VideoAppWithChatbotComponent />
     </div>
